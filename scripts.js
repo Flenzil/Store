1e70fea9e698323f4b5f6cade2cdbc79e5714724
Flenzil <sam.neale100@gmail.com>
2024-05-22 13:36:12 +0100

    feat: add clickable rows in list view to redirect to product page

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
 function productList() {
     document.querySelector("#product-grid").style.display = "none";
     document.querySelector("#product-list").style.display = "block";
+
+    const table = document.querySelector("#product-table");
+
+    for (let row of table.rows) {
+        row.addEventListener("click", () => {
+            location.href = "https://www.myminifactory.com/object/3d-print-rise-of-the-redscales-complete-miniature-pack-330066";
+        })
+    }
 }
 
 function productGrid() {
     document.querySelector("#product-list").style.display = "none";
     document.querySelector("#product-grid").style.display = "";
 }
+
